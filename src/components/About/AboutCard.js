a9d2a3bfe16f305a7fa62e2c6eaa9d12c93d3ab5
seanirbydev <seanirbydev@gmail.com>
2025-05-09 10:52:07 -0400

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,13 +8,12 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Brett Baker </span>
+            from <span className="purple"> Kuala Lumpur, Malaysia.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently employed as a software engineer at Vibhathi Labs.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I have completed Integrated MSc (IMSc) in Maths and Computing at Universiti Malaya.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -34,7 +33,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Brett</footer>
         </blockquote>
       </Card.Body>
     </Card>
